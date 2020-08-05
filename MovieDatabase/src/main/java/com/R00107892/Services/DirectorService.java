11a1e8ec617963e57447216a0bd4b2cb0a1a2c84
Rob <robet.crowley1995@gmail.com>
2020-08-05 22:47:45 +0100

    Rob Changes

--- MovieDatabase/src/main/java/com/R00107892/Services/DirectorService.java
+++ MovieDatabase/src/main/java/com/R00107892/Services/DirectorService.java
@@ -1,16 +1,21 @@
-package com.R00107892.Services;
+package com.R00107892.services;
 
 import java.util.List;
 
-import com.R00107892.Models.Director;
+
+import com.R00107892.entities.Director;
+
 
 public interface DirectorService {
 
-	public List getAllDirectors();
-	 
-    public Director getDirectorById(int id);
- 
-    public void saveOrUpdate(Director director);
- 
-    public void delete(int id);
+    
+    Director findDirector(int directorId);
+	boolean deleteDirector(int directorId);
+	boolean existsByDirectorId(int directorId);
+	String findDirectorName(int directorId);
+	List<Director> findDirectorsWithFilmName(String filmName);
+	Director save(Director director);
+	List<Director> listInAlphabeticalOrder();
+	List<Director> listAllDirectors();
+	boolean deleteDirector(Director director);
 }
